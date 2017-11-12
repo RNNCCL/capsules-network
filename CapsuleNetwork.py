f30bd89153840a737f8bdbb0ffcdad42fd78b6fa
Lei Zhang <lei@nanoleaf.me>
2017-11-12 15:13:41 -0500

    Initial implementation of Capsules Network with basic training.
    No reconstruction cost yet. 3 rerouting iterations.

--- CapsuleNetwork.py
+++ CapsuleNetwork.py
@@ -2,35 +2,99 @@ import tensorflow as tf
 
 
 class CapsuleNetwork(object):
-    def __init__(self, n_inputs, optimizer=tf.train.AdamOptimizer()):
+    def __init__(self, n_inputs, routing_iters=3, optimizer=tf.train.AdamOptimizer(learning_rate=0.001)):
         # self.graph = tf.Graph()
         # with self.graph.as_default():
 
         self.n_inputs = n_inputs
-        self.X = tf.placeholder(tf.float32, (self.n_inputs, 28, 28, 1))
-        self.optimizer = optimizer
+        self.routing_iters = routing_iters
 
-        # build network
-        self.network()
+        self.X_in = tf.placeholder(tf.float32, (self.n_inputs, 784))
+        self.X = tf.reshape(self.X_in, (self.n_inputs, 28, 28, 1))
+        self.Y_in = tf.placeholder(tf.float32, (self.n_inputs, 10))
+        self.Y = tf.reshape(self.Y_in, (self.n_inputs, 1, 10))
+        self.d_capsules = self._network()
+
+        self.cost = self.cost()
+        self.optimizer = optimizer.minimize(self.cost)
 
         init = tf.global_variables_initializer()
         self.sess = tf.Session()
         self.sess.run(init)
 
-    def network(self):
-        with tf.variable_scope('Conv'):
-            conv_layer = tf.contrib.layers.conv2d(self.X, num_outputs=256, kernel_size=9, stride=1, padding='VALID')   # ReLU activation
-            print('conv dims: {}'.format(conv_layer.get_shape()))
+    def _network(self):
+        with tf.variable_scope('Convolution'):
+            convs = tf.contrib.layers.conv2d(self.X, num_outputs=256, kernel_size=9, stride=1, padding='VALID')   # ReLU activation
+            print('conv dims: {}'.format(convs.get_shape()))
 
-        with tf.variable_scope('PrimaryCapsule'):
+        with tf.variable_scope('PrimaryCapsules'):
             # 32 is number of capsule blocks
-            primary_capsule_layer = tf.contrib.layers.conv2d(conv_layer, num_outputs=8 * 32, kernel_size=9, stride=2,
+            p_capsules = tf.contrib.layers.conv2d(convs, num_outputs=8 * 32, kernel_size=9, stride=2,
                                                        padding='VALID', activation_fn=None)    # activation replaced by squashing function
-            print('primary capsule layer dims: {}'.format(primary_capsule_layer.get_shape()))
-            primary_capsule_layer = tf.reshape(primary_capsule_layer, (self.n_inputs, 6, 6, 8, 32)) # last dimension are capsule blocks
-            print("reshaped PC layer dims: {}".format(primary_capsule_layer.get_shape()))
+            print('primary capsule layer dims: {}'.format(p_capsules.get_shape()))
+            p_capsules = tf.reshape(p_capsules, (self.n_inputs, -1, 1, 8, 1))   # add dim for routing
 
         with tf.variable_scope('Routing'):
-            W = tf.get_variable('weight_matrices', shape=(self.n_inputs, ))
+            w = tf.get_variable('weight_matrices', shape=(1, 1152, 10, 8, 16), dtype=tf.float32, initializer=tf.random_normal_initializer())
+
+            u_i = tf.matmul(tf.tile(w, [self.n_inputs, 1, 1, 1, 1]), tf.tile(p_capsules, [1, 1, 10, 1, 1]), transpose_a=True)
+            print('p_capsule output dims: {}'.format(u_i.get_shape()))
+
+            # routing logits
+            b_ij = tf.get_variable('routing_logits', shape=(1, 1152, 10, 1, 1), dtype=tf.float32,
+                                   initializer=tf.zeros_initializer())
+
+            # routing iterations
+            for iter in range(self.routing_iters):
+                with tf.variable_scope('routing_iter' + str(iter)):
+                    # routing coefficients
+                    c_ij = tf.nn.softmax(b_ij, dim=2)
+
+                    # input to digit capsules
+                    s_j = tf.multiply(tf.tile(c_ij, (self.n_inputs, 1, 1, 1, 1)), u_i)
+                    s_j = tf.reduce_sum(s_j, axis=1, keep_dims=True)
+                    # print('d_capsule input dims: {}'.format(s_j.get_shape()))
+
+                    # digit capsule outputs
+                    s_j_norm = tf.norm(s_j, axis=3, keep_dims=True)
+                    v_j = s_j_norm / (1 + s_j_norm) * s_j / tf.square(s_j_norm)
+                    # v_j = 1 - 1/s_j_norm * s_j / tf.square(s_j_norm)    # equivalent form
+                    # print('d_capsule output dims: {}'.format(v_j.get_shape()))
+
+                    # correlate p_capsule and d_capsule outputs
+                    correlations = tf.matmul(u_i, tf.tile(v_j, (1, 1152, 1, 1, 1)), transpose_a=True)
+                    b_ij += tf.reduce_sum(correlations, axis=0, keep_dims=True)
+
+        return(v_j)
+
+    def cost(self):
+        m_plus = tf.constant(0.90, dtype=tf.float32)
+        m_minus = tf.constant(0.10, dtype=tf.float32)
+        cost_lambda = tf.constant(0.50, dtype=tf.float32)
+
+        d_caps_norm = tf.norm(self.d_capsules, axis=3)
+        d_caps_norm = tf.squeeze(d_caps_norm, axis=3)
+
+        print('d_caps_norm dims {}, Y dims {}'.format(d_caps_norm.get_shape(), self.Y.get_shape()))
+
+        total_cost = tf.reduce_sum(tf.multiply(self.Y, tf.square(tf.maximum(0.0, m_plus - d_caps_norm))), axis=2)
+        assert total_cost.get_shape() == (self.n_inputs, 1)
+
+        total_cost += cost_lambda * tf.reduce_sum(tf.multiply(1 - self.Y, tf.square(tf.maximum(0.0, d_caps_norm - m_minus))), axis=2)
+        assert total_cost.get_shape() == (self.n_inputs, 1)
+
+        total_cost = tf.reduce_sum(total_cost) / self.n_inputs
+        print('total cost dim: {}'.format(total_cost.get_shape()))
+        return total_cost
+
+    def fit(self, X, Y):
+        cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict={self.X_in: X, self.Y_in: Y})
+        return cost
+
+
+
+
+
+
 
 
--- CapsuleRunner.py
+++ CapsuleRunner.py
@@ -1,3 +1,23 @@
+from tensorflow.examples.tutorials.mnist import input_data
 import CapsuleNetwork as cn
 
-capsule_network = cn.CapsuleNetwork(10)
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+n_mnist_train = int(mnist.train.num_examples)
+n_mnist_test = int(mnist.test.num_examples)
+
+n_batch_samples = 100
+n_epochs = 10
+n_batches = n_mnist_train // n_batch_samples
+
+capsule_network = cn.CapsuleNetwork(n_inputs=n_batch_samples)
+
+for epoch in range(n_epochs):
+    cost_batch = []
+    for batch in range(n_batches):
+        X_batch, Y_batch = mnist.train.next_batch(n_batch_samples)
+        cost_batch.append(capsule_network.fit(X_batch, Y_batch))
+        print('batch {} ({} of {} training images) cost {:1.3f}'.format(
+            batch, n_batch_samples * (batch+1), n_mnist_train, cost_batch[-1]))
+
+    cost_epoch = sum(cost_batch) / n_batches
+    print('epoch {} avg cost {:1.3f}'.format(epoch, cost_epoch))
